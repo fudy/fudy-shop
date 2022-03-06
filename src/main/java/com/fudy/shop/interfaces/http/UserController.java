e7b53dff8252713e5f3a4cfbb4e012877a57eed9
Fudy <lifudong1@163.com>
2022-03-06 23:07:05 +0800

    add log

--- src/main/java/com/fudy/shop/interfaces/http/UserController.java
+++ src/main/java/com/fudy/shop/interfaces/http/UserController.java
@@ -1,7 +1,9 @@
 package com.fudy.shop.interfaces.http;
 
+import com.alibaba.fastjson.JSON;
 import com.fudy.shop.interfaces.dto.Result;
 import com.fudy.shop.interfaces.dto.UserDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
