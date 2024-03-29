2f21ddfc9f28e9d1d82b3090f86146cda4973821
Fudy <lifudong1@163.com>
2024-03-29 21:47:58 +0800

    save

--- fudy-shop-interfaces/src/main/java/com/fudy/shop/interfaces/http/MainController.java
+++ fudy-shop-interfaces/src/main/java/com/fudy/shop/interfaces/http/MainController.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+
 @Controller
 public class MainController {
-    @GetMapping("/")
+
+    @GetMapping("/*")
     public String index(ModelMap map) {
-        map.addAttribute("message", "Hello World");
-        map.addAttribute("jsFileURL", "https://www.thymeleaf.org/scripts/thymeleaf.js");
+        map.put("cssFiles", Arrays.asList("forget_password.d116754d.css", "index.32f4290a.css", "index.70ca10ae.css", "login.25aa6676.css", "registry.4d3884ed.css"));
+        map.put("jsFiles", Arrays.asList("login.7a2162d3.js", "modify-password-success.df71cf41.js", "registry.c27a8daf.js", "index.63a80766.js", "index.f30a2f53.js", "forget_password.01e99a39.js", "captcha.906660c8.js"));
         return "index";
     }
 }
